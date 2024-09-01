ae395d353f3a23587324261d172ba5e40803536a
Henry-Ruiz <henryruiz686@gmail.com>
2024-09-01 10:46:00 -0400

    5 comit(hasta video 16)

--- vista/asideMenu.php
+++ vista/asideMenu.php
@@ -11,6 +11,7 @@
       <li class="nav-item nav-link">
         <span class="badge badge-danger" id="comunSiat">Desconectado</span>
       </li>
+
     </ul>
 
     <!-- Right navbar links -->
@@ -31,7 +32,7 @@
     <!-- Brand Logo -->
     <a href="inicio" class="brand-link">
       <img src="assest/dist/img/Logo_POS.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Sistema</span>
+      <span class="brand-text font-weight-light">Sistema POS</span>
     </a>
 
     <!-- Sidebar -->
@@ -50,8 +51,6 @@
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-user"></i>
